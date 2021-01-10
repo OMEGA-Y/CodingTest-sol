2da1e7a1771bbfd0d77c5fff20b60e6040e75ed5
OMEGA-Y <science_co@naver.com>
2021-01-10 13:46:38 +0900

    problem 2743 sol python,java

---
+++ solution/wordLength_2743.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class wordLength_2743 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(br.readLine().length());
+	}
+}
